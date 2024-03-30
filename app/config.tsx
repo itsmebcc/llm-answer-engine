3551499c167a78c0c5826addfafe9de2ea82de37
itsmebcc <81497332+itsmebcc@users.noreply.github.com>
2024-03-30 15:40:24 -0600

    Update config.tsx
    Update to allow for Follow-up from Local LLM

--- app/config.tsx
+++ app/config.tsx
@@ -6,13 +6,14 @@
 
 export const config = {
     useOllamaInference: false, 
-    useOllamaEmbeddings: false, 
+    useOllamaEmbeddings: true, 
     inferenceModel: 'mixtral-8x7b-32768', // Groq: 'mixtral-8x7b-32768', 'gemma-7b-it' // OpenAI: 'gpt-3.5-turbo', 'gpt-4' // Ollama 'mistral', 'llama2' etc
     inferenceAPIKey: process.env.GROQ_API_KEY, // Groq: process.env.GROQ_API_KEY // OpenAI: process.env.OPENAI_API_KEY // Ollama: 'ollama' is the default
-    embeddingsModel: 'text-embedding-3-small', // Ollama: 'llama2', 'nomic-embed-text' // OpenAI 'text-embedding-3-small', 'text-embedding-3-large'
-    textChunkSize: 1000, // Recommended to decrease for Ollama
-    textChunkOverlap: 400, // Recommended to decrease for Ollama
+    embeddingsModel: 'qwen:latest', // Ollama: 'llama2', 'nomic-embed-text' // OpenAI 'text-embedding-3-small', 'text-embedding-3-large'
+    textChunkSize: 600, // Recommended to decrease for Ollama
+    textChunkOverlap: 200, // Recommended to decrease for Ollama
     numberOfSimilarityResults: 4, // Numbher of similarity results to return per page
     numberOfPagesToScan: 10, // Recommended to decrease for Ollama
-    nonOllamaBaseURL: 'https://api.groq.com/openai/v1', //Groq: https://api.groq.com/openai/v1 // OpenAI: https://api.openai.com/v1 
+    nonOllamaBaseURL: 'http://localhost:5000/v1', //Groq: https://api.groq.com/openai/v1 // OpenAI: https://api.openai.com/v1 
+//    nonOllamaBaseURL: 'https://api.groq.com/openai/v1', //Groq: https://api.groq.com/openai/v1 // OpenAI: https://api.openai.com/v1 
 };
